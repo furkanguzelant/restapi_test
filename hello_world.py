2abe7a8e651e638c559022b27682f8a96f93cf37
furkanguzelant <72248390+furkanguzelant@users.noreply.github.com>
2024-11-15 02:22:04 +0300

    Add or update file hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,6 @@
 # CS 421 Programming Assignment 1 - PseudoGit
-
 def greet_the_student():
-     print("21901515")
-
+     print("Furkan")
 if(__name__ == "__main__"):
      print(greet_the_student())
             
