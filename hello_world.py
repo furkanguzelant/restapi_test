2a0c1a5a6eed338f2f52d42ee19b1b5669b0c02c
furkanguzelant <72248390+furkanguzelant@users.noreply.github.com>
2024-11-20 03:03:12 +0300

    uploading file

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,6 @@
 # CS 421 Programming Assignment 1 - PseudoGit
 def greet_the_student():
-     print("hello")
+     print("Furkan")
 if(__name__ == "__main__"):
      print(greet_the_student())
             
